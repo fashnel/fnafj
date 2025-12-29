6c024fb92090cd26da4b1eb058ddd8dce551a363
polina <you@example.com>
2025-12-29 11:20:42 +0300

    fixed name window

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 class Main {
     public static void main(String[] args) {
         JFrame window = new JFrame();
-        window.setTitle("Onw Shift in Storage");
+        window.setTitle("One Shift in Storage");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
 
